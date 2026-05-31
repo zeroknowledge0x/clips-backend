30289cabdc7528a28b50c670b0b4245395941a2d
Noble <tabss56@outlook.com>
2026-05-31 19:21:55 +0000

    create queue

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
+import { QueueCleanupService } from './queue-cleanup.service';
 import { CLIP_GENERATION_QUEUE } from '../clips/clip-generation.queue';
 
 @Module({
   imports: [BullModule.registerQueue({ name: CLIP_GENERATION_QUEUE })],
   controllers: [JobsController],
-  providers: [JobsService],
+  providers: [JobsService, QueueCleanupService],
 })
 export class JobsModule {}
